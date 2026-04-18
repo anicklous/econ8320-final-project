bfd5e267fc0127094bc345da734d1135e0f90022
anicklous <anicklous@unomaha.edu>
2026-04-18 16:11:31 +0000

    Changed Title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("ECON 8320 Final Project")
+st.write("A Streamlit dashboard updated monthly with BLS data")
+
