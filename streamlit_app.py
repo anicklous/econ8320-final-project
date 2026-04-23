f64774cc6d7407355f633ccc27af815a144a2517
anicklous <anicklous@unomaha.edu>
2026-04-23 22:50:25 +0000

    Read in CSV files

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,4 +14,12 @@ st.sidebar.page_link("https://www.bls.gov/developers/api_signature_v2.htm",label
 st.sidebar.page_link("https://data.bls.gov/toppicks?survey=bls",label=":blue[Original data source]")
 
 st.title("📈ECON 8320 Final Project📉")
-st.write("Updated monthly with published data from the Bureau of Labor Statistics")
+st.write("A Streamlit Dashboard updated monthly with current data from the Bureau of Labor Statistics")
+
+avgHourly = pd.read_csv("https://raw.githubusercontent.com/anicklous/econ8320-final-project/refs/heads/main/avgHourly.csv")
+exportIndex = pd.read_csv("https://raw.githubusercontent.com/anicklous/econ8320-final-project/refs/heads/main/exportIndex.csv")
+importIndex = pd.read_csv("https://raw.githubusercontent.com/anicklous/econ8320-final-project/refs/heads/main/importIndex.csv")
+nonfarm = pd.read_csv("https://raw.githubusercontent.com/anicklous/econ8320-final-project/refs/heads/main/nonfarm.csv")
+unempRate = pd.read_csv("https://raw.githubusercontent.com/anicklous/econ8320-final-project/refs/heads/main/unempRate.csv")
+
+st.line_chart(avgHourly,x= 'periodName',y='value')
